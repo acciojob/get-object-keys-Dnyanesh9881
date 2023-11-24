823b36c7045b29c4c49b8584f040ac9039c5f640
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-11-24 16:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const student={
+	name:'gaju',
+}
+Object.prototype.getKeys=function () {
+	let arr=[];
+	let j=0;
+	for(let i in this){
+		arr[j]=i;
+		j++;
+	}
+	return arr;
+}
