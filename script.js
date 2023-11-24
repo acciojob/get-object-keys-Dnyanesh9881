0a28193374ccc94a7c76a62e7378a22223e9dd80
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-11-24 16:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const student={
 Object.prototype.getKeys=function () {
 	let arr=[];
 	let j=0;
-	for(let i in this){
+	for(let i in student){
 		arr[j]=i;
+		console.log()
 		j++;
 	}
 	return arr;
